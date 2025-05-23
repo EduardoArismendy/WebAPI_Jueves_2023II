20516b2c27f3db9bc5d1572bad09455a2f35836e
EduardoArismendy <p73306275@gmail.com>
2025-05-23 14:37:22 -0500

    Avance de Domain 1

---
+++ Shopping_API_Jueves_práctica/Shopping_API_Jueves_práctica/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,6 @@
+﻿namespace Shopping_API_Jueves_práctica.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+    }
+}
---
+++ Shopping_API_Jueves_práctica/Shopping_API_Jueves_práctica/Domain/Services/CountryService.cs
@@ -0,0 +1,6 @@
+﻿namespace Shopping_API_Jueves_práctica.Domain.Services
+{
+    public class CountryService
+    {
+    }
+}
